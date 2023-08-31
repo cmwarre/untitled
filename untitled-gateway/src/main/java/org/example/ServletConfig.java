a3aff7cc64e241965fb6fa82da5f822606103b2a
Cody Warren <cmwarre@users.noreply.github.com>
2023-08-31 09:15:16 -0600

    Cleaning up files, but Spring web works!

--- untitled-gateway/src/main/java/org/example/ServletConfig.java
+++ untitled-gateway/src/main/java/org/example/ServletConfig.java
@@ -2,11 +2,9 @@ package org.example;
 
 import com.inductiveautomation.ignition.gateway.model.GatewayContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
@@ -22,12 +20,6 @@ public class ServletConfig extends WebMvcConfigurationSupport {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter());
-    }
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer
-                .defaultContentType(MediaType.APPLICATION_JSON);
     }
 
 }
